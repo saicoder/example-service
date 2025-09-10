0261395c05a22e1e42f2eacd4dfba924a087d963
Branko <saicoder.net@gmail.com>
2025-09-11 00:32:48 +0200

    Add simple env

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
+import os
+
+var = os.getenv('SYSTEM_PROMPT', 'NOT PROVIDED!!!!!')
 
 i = 1
 while True:
-    print(f"Running service {i}")
+    print(f"Running service {i}: {var}")
     time.sleep(3)
     i += 1
